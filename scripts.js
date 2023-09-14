08907e61b0753745acae1bccceec1c88f158bd38
dtesters <144807230+dtesters@users.noreply.github.com>
2023-09-14 11:12:31 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Sticky navigation bar
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 100) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
